c0fa5a0cc004ffbf613c79a0b6e276f500c1251a
kinderror <87951557+kinderror@users.noreply.github.com>
2022-06-09 19:36:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
+#include <cmath>
 
+using namespace std;
+
+// ......const values......
+const float delta = 0.001;
+const int a = 10, r = 1, v_mean = 1;
+
+// ......forward declaration......
 struct Cell;
 struct Particle;
-void create_cells(Cell **cell_pointers, int cell_number);
-void create_particles(Particle **particle_pointers, int particle_number);
+void create(Cell **cell_p, Particle **part_p);
+void iteration(Cell **cell_p, Particle **part_p);
 
+// ......main......
 int main() {
-    int cell_number = 100, particle_number = 100;
-    Cell **cell_pointers = new Cell *[cell_number];
-    create_cells(cell_pointers, cell_number);
-    Particle **particle_pointers = new Particle *[particle_number];
-    create_particles(particle_pointers, particle_number);
+    Cell **cell_p = new Cell *[a * a];
+    Particle **part_p = new Particle *[a * a];
+    create(cell_p, part_p, a, v_mean);
+    iteration(cell_p, part_p, a, delta);
 }
 
+// ......structures.......
 struct Cell {
-    int x;
-    int y;
-    Particle **particles;
-    Cell(int val_x, int val_y) {
-        x = val_x;
-        y = val_y;
-        particles = new Particle *[0];
+    Particle **inner;
+    Particle **outer;
+    Cell() {
+        inner = new Particle *[0];
+        outer = new Particle *[0];
     }
 };
 
 struct Particle {
+    int index;
     float x;
     float y;
     float vx;
     float vy;
-    Cell **cells;
-    Particle(float val_x, float val_y, float val_vx, float val_vy) {
+    Particle(int i, float val_x, float val_y, float val_vx, float val_vy) {
+        index = i;
         x = val_x;
         y = val_y;
         vx = val_vx;
         vy = val_vy;
-        cells = new Cell *[0];
     }
+    
 };
 
-void create_cells(Cell **cell_pointers, int cell_number) {
-    for (int i = 0; i < cell_number; i++) {
-        Cell *p = new Cell()
+// ......functions......
+void create(Cell **cell_p, Particle **part_p) {
+    float x, y, vx, vy;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dist(-v_mean, v_mean);
+    for (int i = 0; i < a * a; i++) {
+        Cell *p = new Cell();
+        cell_p[i] = p;
+        x = 10 * (1 + i % a) - 5;
+        y = 10 * (1 + i / a) - 5;
+        vx = dist(gen);
+        vy = pow(v_mean * v_mean - vx * vx, 0.5);
+        Particle *p = new Particle(i, x, y, vx, vy);
+        part_p[i] = p;
     }
 }
 
-void create_particles(Particle **particle_pointers, int particle_number) {
-    
+void iteration(Cell **cell_p, Particle **part_p) {
+    int cell_index;
+    for (int i = 0; i < a * a; i++) {
+        cell_index = floor(x / 10) + a * floor(y / 10);
+        cell_p[cell_index] 
+    }
 }
 
